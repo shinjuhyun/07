c862a71989293d437f236b7ac04df6ab546d5da8
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-10-18 16:50:25 +0900

    04

--- main.c
+++ main.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int inc(int counter);
- 
-int main(void) {
-	int i = 10;
-	printf("함수 호출전 i=%d\n", i);
-	inc(i);
-	printf("함수 호출후 i=%d\n",i);
-	return 0;
-}
-int inc(int counter)
+void f(void);
+int i; 
+int main(void) 
 {
-	counter++;
-	return counter;
+	for(i=0;i<5;i++)
+	{
+		f();
+	}
+	return 0;	
+}
+void f(void)
+{
+	for(i=0;i<10;i++)
+		printf("#");
 }
