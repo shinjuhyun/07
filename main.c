d1ccdcf47283cde29db9d0fa88c2032cde42bdca
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-10-18 17:11:09 +0900

    6-1

--- main.c
+++ main.c
@@ -2,24 +2,14 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void sub(void);
+int all_files;
+
+extern void sub();
  
 int main(void) 
 {
-	int i;
-	for(i=0;i<3;i++)
-		sub();
+	sub();
+	printf("%d\n",all_files);
 	return 0;	
 }
-void sub(void)
-{
-	int auto_count = 0;//자동할당 
-	static int static_count = 0;//정적할당 
-	//변수 1 증가  
-	auto_count++;
-	static_count++;
-	
-	//변수 내용 확인 
-	printf("auto_count=%d\n",auto_count);
-	printf("static_count=%d\n",static_count);
-}
+
